3fa8c084fea271c22f5278d4b2696b199afb136e
Mitry <isorrowno@gmail.com>
2023-05-10 17:59:34 -0300

    feat: gunicorn

---
+++ back/proj_back/conf/gunicorn_conf.py
@@ -0,0 +1,7 @@
+from multiprocessing import cpu_count
+workers = cpu_count() * 2 + 1
+limit_request_fields = 32000
+limit_request_field_size = 0
+user = 'root'
+raw_env = 'DJANGO_SETTINGS_MODULE=proj_back.settings'
+
