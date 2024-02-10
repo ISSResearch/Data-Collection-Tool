c7b417df22c4b8c0fdda311f41be587e312bfd6c
Mitry <isorrowno@gmail.com>
2024-02-10 09:02:56 -0300

    ref: front annotation

--- frontend-app/src/utils/index.js
+++ frontend-app/src/utils/index.js
@@ -1,12 +1,18 @@
 import { TYPES_MAP } from "../config/settings";
 
+/**
+* @param {object} File
+* @param {string} File.type
+* @param {string} File.name
+* @returns {{ name: string, extension: string, type: string }}
+*/
 export function extractFileMeta({ type: fileType, name: fileName }) {
   var [typeName, typeExt] = fileType.split('/');
   var nameSplit = fileName.split('.');
 
   var extension;
   if (nameSplit.length > 1) {
-    var nameExt = nameSplit.pop()
+    var nameExt = nameSplit.pop();
     extension = typeExt || nameExt;
   }
   else extension = typeExt || '';
@@ -17,11 +23,18 @@ export function extractFileMeta({ type: fileType, name: fileName }) {
   return { name, extension, type };
 }
 
+/**
+* @returns {string}
+*/
 export function getOriginDomain() {
   var { origin } = window.location;
   return origin.split(':', 2).join(':');
 }
 
+/**
+* @param {any} obj
+* @returns {any}
+*/
 export function deepCopy(obj) {
   if (typeof obj !== 'object' || obj === null) return obj;
   var isArray = Array.isArray(obj);
@@ -32,12 +45,24 @@ export function deepCopy(obj) {
     }, isArray ? [] : {});
 }
 
+/**
+* @param {object[]} arr
+* @param {(string | number)[]} indexes
+* @param {number} pointer
+* @returns {object[]}
+*/
 export function deepFind(arr, indexes, pointer = 0) {
   return pointer < indexes.length - 1
     ? deepFind(arr[indexes[pointer]].children, indexes, pointer + 1)
     : arr[indexes[pointer]];
 }
 
+/**
+* @param {object[]} node
+* @param {string} parentPath
+* @param {number} changeIndex
+* @returns {undefined}
+*/
 export function refreshPath(node, parentPath = null, changeIndex = null) {
   node.forEach((item, index) => {
     if (changeIndex && index < changeIndex) return;
@@ -53,8 +78,15 @@ export function refreshPath(node, parentPath = null, changeIndex = null) {
   });
 }
 
+/**
+* @returns {number}
+*/
 export function formUID() { return Number(Math.random().toFixed(16).slice(2)); }
 
+/**
+* @param {object[]} items
+* @returns {object[]}
+*/
 export function findRequired(items) {
   var requiredItems = [];
   var stack = [...items];
@@ -68,6 +100,11 @@ export function findRequired(items) {
   return requiredItems;
 }
 
+/**
+* @param {string} fileName
+* @param {object[]} missedItems
+* @returns {{ isValid: boolean, message?:string }}
+*/
 export function formError(fileName, missedItems) {
   if (!missedItems.length) return { isValid: true };
 
@@ -78,12 +115,19 @@ export function formError(fileName, missedItems) {
   };
 }
 
+/**
+* @param {object} data
+* @param {boolean} refresh
+* @returns {object[]}
+*/
 export function spreadChildren(data, refresh = true) {
   if (refresh) refreshPath(data);
-  const stack = [data];
-  const result = [];
+
+  var stack = [data];
+  var result = [];
+
   while (stack.length) {
-    const current = stack.pop();
+    var current = stack.pop();
     result.push(...current);
     stack.push(
       ...current
@@ -94,6 +138,14 @@ export function spreadChildren(data, refresh = true) {
   return result;
 }
 
+/**
+* @param {object[]} items
+* @param {string} lookUpKey
+* @param {string} lookUpVal
+* @param {string} replaceWith
+* @param {boolean} head
+* @returns {undefined}
+*/
 export function traverseWithReplace(
   items,
   lookUpKey,
@@ -112,6 +164,11 @@ export function traverseWithReplace(
   });
 }
 
+/**
+* @param {object[]} items
+* @param {number} parentOrder
+* @returns {undefined}
+*/
 export function drawPaths(items, parentOrder=0) {
   items.forEach((item, index) => {
     var { children } = item;
