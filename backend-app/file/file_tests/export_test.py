747485e6e1be17afc9452915dfe5ee5d0563e73b
Mitry <isorrowno@gmail.com>
2024-07-16 17:22:29 -0300

    fix

--- backend-app/file/file_tests/export_test.py
+++ backend-app/file/file_tests/export_test.py
@@ -1,4 +1,4 @@
-from file.export import XLS, JSON, CSV
+from file.export import JSON, CSV
 from file.services import StatsServices
 from django.test import TestCase
 from attribute.attribute_tests.mock_attribute import MockCase
@@ -13,11 +13,8 @@ class ExportTest(TestCase):
         cls.attr_stat, _ = StatsServices.from_attribute(cls.case.project.id)
         cls.user_stat, _ = StatsServices.from_user(cls.case.project.id)
 
-    def test_xls(self):
-        ...
-        #attr_res = XLS(self.attr_stat, "attribute").into_response()
-        #user_res = XLS(self.user_stat, "user").into_response()
-        # TODO:
+    # TODO:
+    def test_xls(self): ...
 
     def test_csv(self):
         attr_res = CSV(self.attr_stat, "attribute").into_response()
